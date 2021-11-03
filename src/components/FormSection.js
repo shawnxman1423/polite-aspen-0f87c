5db9a44aa79fb776d2ebd80d2580f879f04bdadc
Stackbit Code Editor <projects@stackbit.com>
2021-11-03 21:26:27 +0000

    FormSection.js: changed

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -51,6 +51,8 @@ export default class FormSection extends React.Component {
         if (((form_field_count < 2) && (form_layout === 'inline'))) {
              form_is_inline = true;
         }
+
+        let form_fields = _.get(section, 'form_fields', null)
         return (
             <React.Fragment>
                 <section className={classNames('section', {'has-border': _.get(section, 'has_border', null), 'has-cover': _.get(section, 'background_image', null), 'bg-none': bg_color === 'none', 'bg-primary': bg_color === 'primary', 'bg-secondary': bg_color === 'secondary', 'pt-4': padding_top === 'small', 'pt-6': (padding_top === 'medium') || (padding_top === 'large'), 'pt-md-7': padding_top === 'large', 'pb-4': padding_bottom === 'small', 'pb-6': (padding_bottom === 'medium') || (padding_bottom === 'large'), 'pb-md-7': padding_bottom === 'large'})}>
