26fe265ce2acedb768f9ccf17776015fa1a7ae54
Stackbit Code Editor <projects@stackbit.com>
2021-11-03 20:10:02 +0000

    GridItem.js: changed

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -92,9 +92,7 @@ export default class GridItem extends React.Component {
                 					{_.get(item, 'actions', null) && (
                 					<div className={classNames('item__actions', 'btn-group', {'btn-group--fill': actions_width === 'full-width', 'justify-center': actions_align_x === 'center', 'justify-end': actions_align_x === 'right'})}>
                                         <SectionActions {...this.props} actions={_.get(item, 'actions', null)} />
-                                        <Paypal 
-                                                amount={36}
-                                                currency="USD"
+                                        <Paypal amount={36} currency="USD"
                                         />
                 					</div>
                                     )}
