0abb1bed862b51ac507400b63c603e167b57b0fa
Stackbit Code Editor <projects@stackbit.com>
2021-11-04 08:58:03 +0000

    FormSection.js: changed

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -8,7 +8,8 @@ import Paypal from "gatsby-plugin-paypal"
 export default class FormSection extends React.Component {
     render() {
         let form_id = _.get(section, 'form_id', null)
-        
+        var amount = 0
+        if(form_id == "buy_pro")
         const createOrder = (data, actions) => {
             return actions.order
             .create({
