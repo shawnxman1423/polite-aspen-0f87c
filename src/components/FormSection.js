c5ab2b71b92311a0a346685446e1952921dc4205
Stackbit Code Editor <projects@stackbit.com>
2021-11-04 08:55:34 +0000

    FormSection.js: changed

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -15,7 +15,7 @@ export default class FormSection extends React.Component {
                     amount: {
                         value: 36,
                     },
-                },
+                }
                 ],
                 // remove the application_context object if you need your users to add a shipping address
                 application_context: {
