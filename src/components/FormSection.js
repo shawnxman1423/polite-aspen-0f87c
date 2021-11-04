cd24a2e59defcbd98b848327088da2281923f089
Stackbit Code Editor <projects@stackbit.com>
2021-11-04 08:15:03 +0000

    FormSection.js: changed

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -6,6 +6,40 @@ import FormField from './FormField';
 import Paypal from "gatsby-plugin-paypal"
 
 export default class FormSection extends React.Component {
+    const createOrder = (data, actions) => {
+            return actions.order
+            .create({
+                purchase_units: [
+                {
+                    amount: {
+                        value: 36,
+                    },
+                },
+                ],
+                // remove the application_context object if you need your users to add a shipping address
+                application_context: {
+                    shipping_preference: 'NO_SHIPPING',
+                },
+            })
+            .then((orderID) => {
+                //setOrderID(orderID);
+                return orderID;
+            });
+        };
+
+        // handles when a payment is confirmed for paypal
+        const onApprove = (data, actions) => {
+            return actions.order
+            .capture()
+            .then(function (details) {
+                const { payer } = details;
+                //setSucceeded(true);
+                //PayedEvent();
+            })
+            .catch((err) => { 
+                //setPaypalErrorMessage('Something went wrong.')
+            });
+        };
     render() {
         let section = _.get(this.props, 'section', null);
         let padding_top = _.get(section, 'padding_top', null) || 'medium';
